6f5ecaee32058cb7ba47ffc63295bc0cbd9a00a5
agathemmanuel <agathemmanuel00@gmail.com>
2022-03-12 22:32:20 +0530

    Adding unique id to payloads in post request and returning id

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from random import randrange
+from typing import Optional
 import pydantic
 from fastapi import FastAPI
 from fastapi.params import Body
@@ -10,21 +12,32 @@ class Post(BaseModel):
     description: str
     content: bool=False
     date: str="one"
+    rating: Optional[int]=None
 
+sample_posts=[{"title":"title1","description":"description1","content":"content1","date":"date1","rating":"rating1"},{"title":"title2","description":"description2","content":"content2","date":"date2","rating":"rating2"}]
 
 @app.get("/")
 def root():
+    print()
     return {"message": "Helloooooo Worldddddddddddd"}
 
 
-@app.get("/posts")
+
+@app.get("/post")
 def get_posts():
-    return {"data": "display you posts "}
+    print("get_post initiated")
+    return {"message": sample_posts}
 
 
-@app.get("/createpost")
+
+@app.post("/post")
 def create_post(payload: Post):
     print(payload)
-    return {"data": "new post"}
+    print(payload.dict())
+    payload_dict= payload.dict()
+    payload_dict['id']=randrange(0,1000000)
+    sample_posts.append(payload_dict)
+    print(sample_posts)
+    return {"data": f"returning id of new post created with unique_id: {payload_dict['id']} title: {payload_dict['title']} content: {payload_dict['content']}" }
 
 
