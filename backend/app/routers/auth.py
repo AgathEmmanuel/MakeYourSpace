5f1215b57f6617c8b88ed6a3020a1c7f8b5d6940
agathemmanuel <agathemmanuel00@gmail.com>
2022-03-20 16:33:07 +0530

    Added OAuth2PasswordRequestForm and functionality to authenticate with username or email

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
+from sqlalchemy import or_
 from database import engine, get_db
 import schemas
 import models
 from routers import oauth2
 from passlib.context import CryptContext
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm  
+
 
 
 password_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
@@ -17,12 +20,12 @@ router = APIRouter(
 
 
 @router.post('/')
-def login_user(payload: schemas.UserLogin, db: Session = Depends(get_db)): 
-    user_info = db.query(models.Accounts).filter(models.Accounts.email==payload.email).first()
+def login_user(payload: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)): 
+    user_info = db.query(models.Accounts).filter(or_(models.Accounts.email==payload.username,models.Accounts.username==payload.username)).first()
     if not user_info:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"The Credentials are invalid")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"The Credentials are invalid with no user info")
     if not password_context.verify(payload.password,user_info.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"The Credentials are invalid")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"The Credentials are invalid with password filure")
 
     jwt_token = oauth2.create_access_token(data={"user_id": user_info.username})
     return {"token": jwt_token,"token_type": "bearer"}
